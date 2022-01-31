ce54f509dcb6f3b57ed720c4d0782992348549ed
lemuellloren <lempakz@yahoo.com>
2022-01-31 23:12:13 +0800

    set up mobile menu

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,3 +1,27 @@
 <template>
-    <h1>Hello World!</h1>
+  <nav>
+    <div class="container">
+      <!-- * Mobile Menu  -->
+      <div class="mobile-menu d-block d-sm-none">
+        <div class="mobile-menu__row row d-flex justify-content-center align-items-center">
+          <div class="col-2">
+            <div class="hamburger-menu">
+              <span class="fas fa-bars"></span>
+            </div>
+          </div>
+          <div class="col-8">
+            <div class="logo">
+              <img class="logo__img" src="~/assets/images/logo.png" alt="VERMA FARMS">
+            </div>
+          </div>
+          <div class="col-2">
+            <div class="bag">
+              <img class="bag__icon" src="~/assets/images/svg-gobbler.svg" alt="Bag">
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- * Mobile Menu  -->
+    </div>
+  </nav>
 </template>
--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,8 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.15.4/css/all.css', integrity: 'sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm', crossorigin: 'anonymous'}
     ]
   },
 
