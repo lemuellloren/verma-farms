4172324412844082431864a586ed476196908099
lemuellloren <lempakz@yahoo.com>
2022-02-01 00:05:45 +0800

    create offcanvas sidebar menu mobile

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,12 +1,12 @@
 <template>
-  <nav>
+  <nav id="nav">
     <div class="container">
       <!-- * Mobile Menu  -->
-      <div class="mobile-menu d-block d-sm-none">
+      <div id="mobileMenu" class="mobile-menu d-block d-sm-none">
         <div class="mobile-menu__row row d-flex justify-content-center align-items-center">
           <div class="col-2">
-            <div class="hamburger-menu">
-              <span class="fas fa-bars"></span>
+            <div id="hamburgerMenu" class="hamburger-menu">
+              <span v-on:click="openNav" class="hamburger-menu__icon fas fa-bars"></span>
             </div>
           </div>
           <div class="col-8">
@@ -23,5 +23,49 @@
       </div>
       <!-- * Mobile Menu  -->
     </div>
+    <!-- OffCanvas  -->
+    <div class="sidebar-menu" id="sideBar">
+      <div class="sidebar-menu__container">
+          <span v-on:click="closeNav" class="close-nav fas fa-chevron-right"></span>
+          <div class="mobile-menu-logo">
+              <img src="~/assets/images/verma-green-icon.png" alt="Verma Farm" class="mobile-menu-logo__img">
+          </div>
+        <ul class="sidebar-menu__ul">
+          <li class="sidebar-menu__li">
+            Shop
+          </li>
+          <li class="sidebar-menu__li">
+            Our Story
+          </li>
+          <li class="sidebar-menu__li">
+            Learn
+          </li>
+          <li class="sidebar-menu__li">
+            Community
+          </li>
+          <li class="sidebar-menu__li">
+            Blog
+          </li>
+        </ul>
+      </div>
+    </div>
+    <!-- OffCanvas  -->
   </nav>
 </template>
+
+<script>
+  export default {
+    el: '#nav',
+    methods: {
+      openNav: function () {
+        document.getElementById('sideBar').style.width = "250px";
+        document.getElementById('mobileMenu').style.marginLeft = "250px";
+      },
+      closeNav: function () {
+        document.getElementById("sideBar").style.width = "0";
+        document.getElementById("mobileMenu").style.marginLeft = "0";
+      }
+    }
+  }
+
+</script>
